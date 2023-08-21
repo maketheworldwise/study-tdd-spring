67863365ff1efd30d9b71e0bcc588401ea353608
Kevin Ahn <maketheworldwise@gmail.com>
2023-08-21 22:58:21 +0900

    init: tada

---
+++ src/main/java/com/example/product/ProductAdapter.java
@@ -0,0 +1,17 @@
+package com.example.product;
+
+import org.springframework.stereotype.Component;
+
+@Component
+class ProductAdapter implements ProductPort {
+	private ProductRepository productRepository;
+
+	ProductAdapter(final ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
+
+	@Override
+	public void save(final Product product) {
+		productRepository.save(product);
+	}
+}
---
+++ src/main/java/com/example/product/ProductRepository.java
@@ -0,0 +1,18 @@
+package com.example.product;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+class ProductRepository {
+
+	private Map<Long, Product> persistence = new HashMap<>();
+	private Long sequence = 0L;
+
+	public void save(final Product product) {
+		product.assignId(++sequence);
+		persistence.put(product.getId(), product);
+	}
+}
